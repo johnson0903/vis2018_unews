5a9ccfd0d14a280ed071de50f395a83ffb1df3d4
李嘉晟 <johnson50701@gmail.com>
2018-05-30 17:59:06 +0800

    新增文章

--- routes/emptyhouse.js
+++ routes/emptyhouse.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.sendfile('views/emptyhouse.html');
 });
-
+router.get('/test', function (req, res, next) {
+    res.sendfile('views/index2.html')
+})
 module.exports = router;
